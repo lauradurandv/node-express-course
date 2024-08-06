dc721b25709b502acd050a0948802f179b7a0b6d
lauradurandv <26388264+lauradurandv@users.noreply.github.com>
2024-08-06 13:13:30 -0400

    add missing req

--- 02-express-tutorial/controllers/people.js
+++ 02-express-tutorial/controllers/people.js
@@ -1,11 +1,11 @@
-const {people} = require('../data');
+let {people} = require('../data');
 
-function getPeople(req,res){
+function getAllPeople(req,res){
     res.status(200).json(people)
 }
 
 function addPerson(req,res){
-    const name = req.body.name;
+    const {name} = req.body;
 
     if (!name){
         //400 indicates error on client side
@@ -26,4 +26,57 @@ function addPerson(req,res){
     });
 }
 
-module.exports = {addPerson, getPeople}
+function getPersonByID(req,res){
+    const id = parseInt(req.params.id);
+    const person = people.find( people => people.id === id)
+    
+    if (!person){
+        return res.status(400).json({
+            success: false,
+            message: "User not found"
+        })
+    }
+
+    return res.status(200).json(person)
+}
+
+function updatePerson(req,res){
+    const id = parseInt(req.params.id);
+
+    const person = people.find( person => person.id === id)
+
+    if (!person){
+        return res.status(404).json({
+            success: false,
+            message: "User not found"
+        })
+    }
+
+    const {name} = req.body;
+    person.name = name;
+
+    
+
+    return res.status(201).json(person)
+
+}
+
+function deletePerson(req,res){
+    const id = parseInt(req.params.id);
+    people = people.filter( people => people.id !== id );
+
+    if (!people){
+        return res.status(400).json({
+            success: false,
+            message: "User not found."
+        })
+    }
+
+    return res.status(200).json({
+        success: true,
+        message: "User has been deleted"
+    })
+}
+
+
+module.exports = {addPerson, getAllPeople, getPersonByID,updatePerson,deletePerson}
--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const router = express.Router();
-let {people} = require('../data');
-const {addPerson, getPeople} = require('../controllers/people');
+const {addPerson, getAllPeople, getPerson, getPersonByID, updatePerson, deletePerson} = require('../controllers/people');
 
 
 //get request for people
-router.get('/', getPeople)
+router.get('/', getAllPeople)
 
 //post request to add people
 router.post('/', addPerson)
+
+//post request to get a particular entry from people array
+router.get('/:id', getPersonByID)
+
+//post to update a user name
+router.put('/:id', updatePerson)
+
+//post to delete a user
+router.delete('/:id',deletePerson)
 
 //export everything
 module.exports = router
