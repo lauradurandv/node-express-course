9f9fa19b5f402f8d22bb25356cd8997d13118920
lauradurandv <26388264+lauradurandv@users.noreply.github.com>
2024-08-29 10:58:38 -0400

    adding in eslint and prettier

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,31 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 //set up middleware
-const tasks = require('./routes/task');
-app.use(express.json())
-const connectDB = require('./conectdb/connect');
-require('dotenv').config()
+const tasks = require("./routes/task");
+app.use(express.json());
+const connectDB = require("./conectdb/connect");
+require("dotenv").config();
 //routes
-app.get('/hello', (req,res)=> {
-    res.send('TASK MANAGER APP')
-})
+app.get("/hello", (req, res) => {
+  res.send("TASK MANAGER APP");
+});
 
-app.use('/api/v1/tasks',tasks)
+app.use("/api/v1/tasks", tasks);
 
 //set up server
 const port = 3000;
 
 //function: connect to database then start server
 const start = async () => {
-    try{
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, console.log(`server is listening on port ${port}`))
-    }catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 //start
-start()
-
+start();
